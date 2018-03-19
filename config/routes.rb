a2bdf4131a75c4f3f21a3d2685756f4b7e2f1f4a
deepozzzie <s3234575@student.rmit.edu.au>
2018-03-20 10:32:54 +1100

    indices index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :indices
   resources :patients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'indices#index'
 end
