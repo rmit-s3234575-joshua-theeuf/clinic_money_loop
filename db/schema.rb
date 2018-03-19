79dcdc3518666e56e887a423a013f0b44a00f566
Ubuntu <ubuntu@ip-172-31-11-21.ap-southeast-2.compute.internal>
2018-03-19 23:23:03 +0000

    Add login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319231346) do
+ActiveRecord::Schema.define(version: 20180319232218) do
+
+  create_table "indices", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "patients", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "firstName"
