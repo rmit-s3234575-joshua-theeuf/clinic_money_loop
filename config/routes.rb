dcb62de5d30f2b78067c9f33a8c8098f741a606d
deepozzzie <s3234575@student.rmit.edu.au>
2018-03-20 15:15:12 +1100

    default new patient

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :indices
   resources :patients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'indices#index'
+  root 'patients#new'
 end
